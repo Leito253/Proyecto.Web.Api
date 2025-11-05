dcbf3aae3ab265697f4092449ca3c1d1a2f87508
Leito253 <leonelf.etn12@gmail.com>
2025-11-05 15:46:35 -0300

    DTOs

---
+++ src/Csharp/Proyecto.Core/DTOs/ClienteCreateDTO.cs
@@ -0,0 +1,11 @@
+namespace Proyecto.Modelos.DTOs
+{
+    public class ClienteCreateDTO
+    {
+        public required string DNI { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
+        public required string Email { get; set; }
+        public required string Telefono { get; set; }
+    }
+}
